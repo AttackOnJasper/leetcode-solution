5fe3def4faab6bbe12bc7df18286a8555f2c8517
AttackOnJasper <wangdc2010@gmail.com>
2016-05-29 21:40:35 -0400

    commit problem answers for 141, 165, 168, 189, and 287

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace::std;
 
@@ -85,10 +86,73 @@ int myAtoi(string str) {
     return res * sign;
 }
 
+// 141. Linked list cycle
+bool hasCycle(ListNode *head) {
+    if (head == NULL || head->next == NULL) return false;
+    ListNode *fast = head;
+    ListNode *slow = head;
+    while (fast != NULL && fast->next != NULL){
+        fast = fast->next->next;
+        slow = slow->next;
+        
+        if (fast == slow) return true;
+    }
+    return false;
+}
 
+// 165. Compare Version Numbers
+int compareVersion(string version1, string version2) {
+    char dot;
+    istringstream ss1(version1), ss2(version2);
+    while (ss1 || ss2) {
+        int v1 = 0, v2 = 0;
+        if (ss1) ss1 >> v1 >> dot;
+        if (ss2) ss2 >> v2 >> dot;
+        if (v1 != v2) {
+            return v1 < v2 ? -1 : 1;
+        }
+    }
+    return 0;
+}
+
+// 168. Excel sheet column title
+// 27 -> AA
+// 28 -> AB
+string convertToTitle(int n) {
+    string result;
+    while (n) {
+        result.push_back('A' + (n - 1) % 26);
+        n = (n - 1) / 26;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+// 189. Rotate Array
+void rotate(vector<int>& nums, int k) {
+    if(k%nums.size())
+    {
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k%nums.size());
+        reverse(nums.begin()+k%nums.size(),nums.end());
+    }
+}
+
+// 287. First Bad Version
+bool isBadVersion(int version);
+
+int helper(int first, int last) {
+    if (first == last - 1) return (isBadVersion(first))? first : last;
+    if (isBadVersion(first+(last-first)/2)){
+        return helper(first, first+(last-first)/2);
+    } else return helper(first+(last-first)/2, last);
+}
+
+int firstBadVersion(int n) {
+    return helper(0, n);
+}
+
+// test
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    // cout << myAtoi("-364") << endl;
-    //    std::cout << reverseString("Hello, World!\n");
     return 0;
 }
